59e7c0f5cbdd0f432f488ee543ab91d622d138e5
Prakash089 <88838703+Prakash089@users.noreply.github.com>
2021-08-26 23:33:44 +0530

    Update settings.py

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -123,6 +123,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
